7900b166a2cbc2b7ee972a526eeba6286a260f42
João Vanzuita <joaovanzuita@me.com>
2019-07-25 10:50:59 -0300

    update_activity update

--- instapy/login_util.py
+++ instapy/login_util.py
@@ -258,7 +258,7 @@ def login_user(browser,
         # update server calls
         update_activity(browser,
                         isServerCall=True,
-                        state=('Clicked Login Button'))
+                        state=None)
 
     # Enter username and password and logs the user in
     # Sometimes the element name isn't 'Username' and 'Password'
@@ -282,7 +282,9 @@ def login_user(browser,
 
     # update server calls for both 'click' and 'send_keys' actions
     for _ in range(2):
-        update_activity()
+        update_activity(browser,
+                        isServerCall=True,
+                        state=None)
 
     sleep(1)
 
